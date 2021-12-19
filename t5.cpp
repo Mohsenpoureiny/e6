1da7322cb189a627681f53c3ad25c8dfbfd377e9
Mohsen Poureiny <mohsenpoureiny@outlook.com>
2021-12-20 00:37:45 +0330

    tamrin 5

--- t5.cpp
+++ t5.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
+void binary(int n)
+{
+    int binerry[32];
+    int i = 0;
+    while (n > 0)
+    {
+        binerry[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+    for (int j = i - 1; j >= 0; j--)
+        cout << binerry[j];
+}
+
 int main()
 {
+    int n;
+    cout << "number:";
+    cin >> n;
+    binary(n);
     return 0;
 }
