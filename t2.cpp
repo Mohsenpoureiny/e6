7ec2ddf717c3ee6f627eb1b862d2710b0f599e38
Mohsen Poureiny <mohsenpoureiny@outlook.com>
2021-12-20 00:27:21 +0330

    tamrin 3

--- t2.cpp
+++ t2.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
 using namespace std;
-
+int func(int x)
+{
+    return (3 * x) + 2;
+}
 int main()
 {
+    for (int i = 2; i <= 10; i++)
+    {
+        cout << func(i) << endl;
+    }
+
     return 0;
 }
