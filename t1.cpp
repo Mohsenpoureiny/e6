21500459e6ded0125e4bca38065bbbd2a1a736f8
Mohsen Poureiny <mohsenpoureiny@outlook.com>
2021-12-20 00:16:21 +0330

    tamrin 1

--- t1.cpp
+++ t1.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 
 int main()
 {
+    int n;
+    int sum = 0;
+    cout << "number:";
+    cin >> n;
+    while (n > 0)
+    {
+        sum += (n % 10);
+        n /= 10;
+    }
+    cout << "\n\n"
+         << "sum=" << sum;
+    cout << "\n\n\n"
+         << "press any key to exit...";
+    getch();
     return 0;
 }
