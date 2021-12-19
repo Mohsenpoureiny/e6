6d0b6f394c3963877af24c277db6d364ed80205f
Mohsen Poureiny <mohsenpoureiny@outlook.com>
2021-12-20 00:24:49 +0330

    tamrin 4

--- t4.cpp
+++ t4.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <string>
+#include <conio.h>
 
 using namespace std;
 
 int main()
 {
+    float sum;
+    int count;
+    while (true)
+    {
+        float i;
+        cin >> i;
+        if (i >= 0)
+        {
+            sum = sum + i;
+            count++;
+        }
+        else
+        {
+            break;
+        }
+    }
+    cout << sum / count;
+    getch();
     return 0;
 }
