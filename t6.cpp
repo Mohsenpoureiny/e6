86d1b5a57497d199203695338d5adcb8161044c5
Mohsen Poureiny <mohsenpoureiny@outlook.com>
2021-12-20 00:41:57 +0330

    tamrin 6

--- t6.cpp
+++ t6.cpp
@@ -4,5 +4,19 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int r = 1; r <= n - i; r++)
+        {
+            cout << "*";
+        }
+        for (int l = 1; l <= i; l++)
+        {
+            cout << l;
+        }
+        cout << endl;
+    }
     return 0;
 }
